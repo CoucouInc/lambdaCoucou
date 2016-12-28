2f9c184dfd145d2c12b41db4ff4148270d101152
Grégoire Charvet 黑瓜 <greg+git@geekingfrog.com>
2016-12-28 20:04:06 +0000

    delete and reset factoids

--- src/LambdaCoucou/Command.hs
+++ src/LambdaCoucou/Command.hs
@@ -38,8 +38,10 @@ handleCommand target (T.CoucouCmdFactoid name factoidType) = do
         T.GetFactoid -> sendFactoid target name
         T.IncFactoid -> adjustCounterFactoid succ target name
         T.DecFactoid -> adjustCounterFactoid pred target name
-        T.SetFactoid val -> setFactoid target name factoids val
-        -- T.AugmentFactoid val -> augmentFactoid target name factoids val
+        T.SetFactoid val -> setFactoid target name val
+        T.ResetFactoid val -> resetFactoid target name val
+        T.DeleteFactoid -> deleteFactoid target name
+        T.AugmentFactoid val -> augmentFactoid target name val
 
 sendFactoid :: Text -> Text -> IRC.StatefulIRC T.BotState ()
 sendFactoid target name = do
@@ -58,7 +60,7 @@ sendRandomFactoid :: Text -> V.Vector Text -> IRC.StatefulIRC T.BotState ()
 sendRandomFactoid target facts =
     unless (V.null facts) $
     do genT <- T._stdGen <$> IRC.state
-       idx <- 
+       idx <-
            liftIO $
            STM.atomically $
            do gen <- STM.readTVar genT
@@ -69,39 +71,35 @@ sendRandomFactoid target facts =
        let msg = IRC.Privmsg target (Right factoid)
        IRC.send msg
 
-adjustCounterFactoid :: (Int -> Int)
-                     -> Text
-                     -> Text
-                     -> IRC.StatefulIRC T.BotState ()
+adjustCounterFactoid :: (Int -> Int) -> Text -> Text -> IRC.StatefulIRC T.BotState ()
 adjustCounterFactoid op target name = do
     factoidsT <- T._factoids <$> IRC.state
     queue <- T._writerQueue <$> IRC.state
-    factoids <- 
+    factoids <-
         liftIO $
-        STM.atomically $ do
-            factoids <- STM.readTVar factoidsT
-            let factoids' = Map.adjust (incdecCounter op) name factoids
-            STM.writeTVar factoidsT factoids'
-            factoids' <- STM.readTVar factoidsT
-            if factoids == factoids'
-            then return Nothing
-            else do
-                Queue.writeTBQueue queue factoids'
-                return $ Just factoids'
+        STM.atomically $
+        do factoids <- STM.readTVar factoidsT
+           let factoids' = Map.adjust (incdecCounter op) name factoids
+           STM.writeTVar factoidsT factoids'
+           factoids' <- STM.readTVar factoidsT
+           if factoids == factoids'
+               then return Nothing
+               else do
+                   Queue.writeTBQueue queue factoids'
+                   return $ Just factoids'
     forM_ factoids (const $ sendFactoid target name)
 
 incdecCounter :: (Int -> Int) -> T.Factoid -> T.Factoid
 incdecCounter op (T.Counter n) = T.Counter $! op n
 incdecCounter _ f = f
 
-setFactoid :: Text -> Text -> T.Factoids -> Text -> IRC.StatefulIRC T.BotState ()
-setFactoid target name factoids val =
+setFactoid :: Text -> Text -> Text -> IRC.StatefulIRC T.BotState ()
+setFactoid target name val = do
+    state <- IRC.state
+    let factoidsT = T._factoids state
+    factoids <- liftIO $ STM.readTVarIO factoidsT
     case Map.lookup name factoids of
-        Just (T.Counter _) ->
-            IRC.send $
-            IRC.Privmsg
-                target
-                (Right "Nope ! (There is already a factoid for that.)")
+        Just (T.Counter _) -> IRC.send $ IRC.Privmsg target (Right "Nope ! (There is already a factoid for that.)")
         Just (T.Facts facts) -> do
             let msg =
                     if length facts > 1
@@ -114,25 +112,43 @@ setFactoid target name factoids val =
             let mbCounter = readMaybe (unpack val)
             let fact = maybe (T.Facts (V.singleton val)) T.Counter mbCounter
             liftIO $
-                STM.atomically $ do
-                    factoids <- STM.readTVar factoidsT
-                    let factoids' = Map.insert name fact factoids
-                    STM.writeTVar factoidsT factoids'
-                    Queue.writeTBQueue (T._writerQueue state) factoids'
+                STM.atomically $
+                do factoids <- STM.readTVar factoidsT
+                   let factoids' = Map.insert name fact factoids
+                   STM.writeTVar factoidsT factoids'
+                   Queue.writeTBQueue (T._writerQueue state) factoids'
             IRC.send $ IRC.Privmsg target (Right "C'est noté !")
 
--- augmentFactoid :: Text
---                -> Text
---                -> T.Factoids
---                -> Text
---                -> IRC.StatefulIRC T.BotState ()
--- augmentFactoid target name factoids val =
---     case Map.lookup name factoids of
---         Nothing -> return ()
---         Just fact -> do
---             let factList =
---                     case fact of
---                         T.Facts fs -> fs
---                         T.Counter n -> (V.singleton (pack $ show n))
---             let fact' = T.Facts $ V.snoc factList val
---             return ()
+deleteFactoid :: Text -> Text -> IRC.StatefulIRC T.BotState ()
+deleteFactoid target name = do
+    factoidsT <- T._factoids <$> IRC.state
+    liftIO . STM.atomically $ STM.modifyTVar' factoidsT (Map.delete name)
+    IRC.send $ IRC.Privmsg target (Right "C'est noté !")
+
+-- not quite thread safe, but hell with it
+resetFactoid :: Text -> Text -> Text -> IRC.StatefulIRC T.BotState ()
+resetFactoid target name val = do
+    factoidsT <- T._factoids <$> IRC.state
+    liftIO . STM.atomically $ STM.modifyTVar' factoidsT (Map.delete name)
+    setFactoid target name val
+
+augmentFactoid :: Text -> Text -> Text -> IRC.StatefulIRC T.BotState ()
+augmentFactoid target name val = do
+    factoidsT <- T._factoids <$> IRC.state
+    writerQ <- T._writerQueue <$> IRC.state
+    msg <-
+        liftIO . STM.atomically $
+        do factoids <- STM.readTVar factoidsT
+           case Map.lookup name factoids of
+               Nothing -> return $ "No factoid there yet, use `" <> name <> " = " <> val <> "`"
+               Just fact -> do
+                   let factList =
+                           case fact of
+                               T.Facts fs -> fs
+                               T.Counter n -> V.singleton (pack $ show n)
+                   let fact' = T.Facts $ V.snoc factList val
+                   let factoids' = Map.insert name fact' factoids
+                   STM.writeTVar factoidsT factoids'
+                   Queue.writeTBQueue writerQ factoids'
+                   return "C'est noté"
+    IRC.send $ IRC.Privmsg target (Right msg)
