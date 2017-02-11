65781fd19609201996f8c43f0ece05aa1a3b69d7
Grégoire Charvet 黑瓜 <greg+git@geekingfrog.com>
2017-02-11 17:56:25 +0000

    Handle images and videos

--- src/LambdaCoucou/Url.hs
+++ src/LambdaCoucou/Url.hs
@@ -1,14 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module LambdaCoucou.Url where
+module LambdaCoucou.Url (updateLastUrl, urlInfo) where
 
-import Data.Text (Text, unpack)
+import Data.Text (Text)
+import qualified Data.Text as Text
 
 import Data.Monoid ((<>))
 import Control.Monad.IO.Class (liftIO)
 import Data.Maybe (fromMaybe, maybe)
 import Control.Exception
+import qualified Data.List as List
 import qualified Data.Text.Encoding as Enc
 import qualified Control.Concurrent.STM as STM
 import qualified Network.IRC.Client as IRC
@@ -16,10 +18,12 @@ import qualified LambdaCoucou.Types as T
 import qualified LambdaCoucou.Parser as Parser
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
+import Data.CaseInsensitive (CI)
 
 import qualified Network.HTTP.Client as HTTP
 import qualified Network.HTTP.Client.TLS as HTTPS
 import Network.HTTP.Types.Status (statusCode)
+import Network.HTTP.Types.Header
 import qualified Text.HTML.TagSoup as HTML
 
 
@@ -48,16 +52,41 @@ urlInfo = do
 
 
 grabTitle :: Text -> IO (Maybe Text)
-grabTitle url = handle (\(_ :: SomeException) -> return Nothing) $ do
-    manager <- HTTP.newManager HTTPS.tlsManagerSettings
-    request <- HTTP.parseRequest (unpack url)
-    response <- HTTP.httpLbs request manager
-    putStrLn $ "The status code was: " ++ show (statusCode $ HTTP.responseStatus response)
-    let decodedBody = Enc.decodeUtf8 $ BS.concat $ LBS.toChunks $ HTTP.responseBody response
-    let tags = HTML.parseTags decodedBody
-    let mbTitle = take 1 $ tail $ dropWhile (HTML.~/= HTML.TagOpen ("title" :: Text) []) tags
-    case mbTitle of
-        [] -> return Nothing
-        (title:_) -> do
-            print $ "raw title: " <> HTML.fromTagText title
-            return $ Just (HTML.fromTagText title)
+grabTitle url =
+    handle (\(_ :: SomeException) -> return Nothing) $
+    do manager <- HTTP.newManager HTTPS.tlsManagerSettings
+       request <- HTTP.parseRequest (Text.unpack url)
+       response <- HTTP.httpLbs request manager
+       putStrLn $ "The status code was: " ++ show (statusCode $ HTTP.responseStatus response)
+       print $ "headers: " <> show (HTTP.responseHeaders response)
+       let body = HTTP.responseBody response
+       case contentType (HTTP.responseHeaders response) of
+           Nothing -> return $ parseTitle body -- still try
+           Just t ->
+               let r
+                       | Text.isInfixOf "text" (Text.toLower t) = parseTitle body
+                       | Text.isInfixOf "html" (Text.toLower t) = parseTitle body
+                       | Text.isPrefixOf "image" (Text.toLower t) =
+                           Just "That's an image, can't display that"
+                       | Text.isPrefixOf "video" (Text.toLower t) =
+                           Just "That's a video, can't display that"
+                       | otherwise = Nothing
+               in return r
+
+contentType :: ResponseHeaders -> Maybe Text
+contentType headers =
+    let mbHeader = List.find ((== ("content-type" :: CI BS.ByteString)) . fst) headers
+        hush (Left _) = Nothing
+        hush (Right a) = Just a
+    in case mbHeader of
+           Nothing -> Nothing
+           Just (_, val) -> hush (Enc.decodeUtf8' val)
+
+parseTitle :: LBS.ByteString -> Maybe Text
+parseTitle body =
+    let decodedBody = Enc.decodeUtf8 $ BS.concat $ LBS.toChunks body
+        tags = HTML.parseTags decodedBody
+        mbTitle = tail $ dropWhile (HTML.~/= HTML.TagOpen ("title" :: Text) []) tags
+    in case mbTitle of
+           [] -> Nothing
+           (title:_) -> Just (HTML.fromTagText title)
